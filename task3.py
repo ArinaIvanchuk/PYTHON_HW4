6de5869358dc8db66d24298f7b54e3a25066ebf5
ArinaIvanchuk <109295614+ArinaIvanchuk@users.noreply.github.com>
2022-10-11 15:25:27 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,8 +2,12 @@
 #которая выведет список неповторяющихся элементов исходной последовательности.
 
 num_list = [1,2,3,3,3,4,5,6,6,7,8,9,9,10]
-for i in range(len(num_list)-1):
-    while num_list[i+1] != num_list[i]:
-        print(num_list[i], end = ', ')        
-        break
-print(num_list[-1])
+new_list = []
+for i in num_list:
+    total = 0
+    for j in num_list:
+        if i == j:
+            total+=1
+    if total == 1:
+        new_list.append(i)
+print(new_list)
